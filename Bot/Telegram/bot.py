3d1c7a637dde5341e6ab6458783ac1bf4c7a700f
Lev <levpalcik@gmail.com>
2025-10-18 17:33:59 +0300

    Tg start

--- Bot/Telegram/bot.py
+++ Bot/Telegram/bot.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import os
+from dotenv import load_dotenv
+import telebot
+from telebot import types
+
+load_dotenv(r"/etc/spectralvpn_data/secret.env")
+TOKEN = os.getenv('SPECTRALVPN_TG_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message) :
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text="Зарегистрироватся"))
